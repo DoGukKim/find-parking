2442035990b694bcce65b5926fcc28303a16d17a
DoGukKim <dgkim3811@naver.com>
2023-01-02 20:21:54 +0900

    프레임 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,25 @@
 import { ReactElement, ReactNode } from 'react'
+import { Frame } from './Frame'
 import { Header, HeaderProps } from './Header'
 
 interface LayoutProps extends HeaderProps {
   children: ReactNode
 }
 
+type GetLayoutProps = {
+  page: ReactElement
+  pageProps: Omit<LayoutProps, 'children'>
+}
+
 export const Layout = ({ children, meta }: LayoutProps) => {
   return (
     <>
       <Header meta={meta} />
-      <main>{children}</main>
+      <Frame>{children}</Frame>
     </>
   )
 }
 
-Layout.getLayout = (
-  page: ReactElement,
-  pageProps: Omit<LayoutProps, 'children'>
-) => <Layout {...pageProps}>{page}</Layout>
+Layout.getLayout = ({ page, pageProps }: GetLayoutProps) => (
+  <Layout {...pageProps}>{page}</Layout>
+)
