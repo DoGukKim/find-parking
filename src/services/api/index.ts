41a73dff1ee96bec556a60347d456748f2a4fe37
Choharim <73021666+Choharim@users.noreply.github.com>
2023-01-06 22:28:10 +0900

    refactor: parking api 결과값 없을 때 추가

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -4,7 +4,7 @@ import axiosInstance from './AxiosInterceptor'
 import { ParkingRowResultResponse, ParkingRowResponse, Payload } from '../type'
 import { API_RESULT_CODE, ERROR_MESSAGE } from '../constant'
 
-const isErrorReponse = (
+const isSpecialCaseReponse = (
   response: ParkingRowResponse | ParkingRowResultResponse
 ): response is ParkingRowResultResponse => {
   return (response as ParkingRowResultResponse).RESULT !== undefined
@@ -20,11 +20,12 @@ const getParking = async (payload?: Partial<Payload['getParking']>) => {
 
   if (response.data.ParkingPlace) {
     return response.data.ParkingPlace?.[1]?.row
-  } else if (
-    isErrorReponse(response.data) &&
-    response.data.RESULT.CODE?.includes(API_RESULT_CODE.ERROR.type)
-  ) {
-    throw response.data.RESULT
+  } else if (isSpecialCaseReponse(response.data)) {
+    if (response.data.RESULT.CODE?.includes(API_RESULT_CODE.ERROR.type)) {
+      throw response.data.RESULT
+    } else {
+      throw new Error(ERROR_MESSAGE.noResult)
+    }
   } else {
     throw new Error(ERROR_MESSAGE.server)
   }
