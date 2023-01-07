1c683a802aeb779bfa5634576b68739be4cea115
Choharim <73021666+Choharim@users.noreply.github.com>
2023-01-08 00:08:45 +0900

    refactor: icon 추가

--- src/components/icon/constant.ts
+++ src/components/icon/constant.ts
@@ -1,5 +1,13 @@
 import MapPin from 'public/icons/map-pin.svg'
+import CurrentLocation from 'public/icons/current-location.svg'
+import GPSSignal from 'public/icons/gps-signal.svg'
+import ParkingMarkEmpty from 'public/icons/parking-mark_empty.svg'
+import ParkingMarkFill from 'public/icons/parking-mark_fill.svg'
 
 export const ICON_COMPONENT = {
   MapPin,
+  CurrentLocation,
+  GPSSignal,
+  ParkingMarkEmpty,
+  ParkingMarkFill,
 } as const
